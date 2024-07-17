a1fc6097dbdd6d71e854792657a2214a71dc680b
Igor Aleksanov <popzxc@yandex.ru>
2024-07-17 10:17:18 +0400

    Fix ws publish --dry-run

--- cargo-workspaces/tests/utils.rs
+++ cargo-workspaces/tests/utils.rs
@@ -1,5 +1,6 @@
 #![allow(dead_code)]
 use assert_cmd::Command;
+use regex::Regex;
 use std::str::from_utf8;
 
 pub fn run(dir: &str, args: &[&str]) -> (String, String) {
@@ -28,4 +29,23 @@ pub fn run_err(dir: &str, args: &[&str]) -> String {
 
     assert!(out.is_empty());
     err
+}
+
+/// Makes the output of commands suitable for snapshot testing:
+/// - Removes the error for missing `http.cainfo` config value.
+/// - Removes `cargo build` output.
+pub fn normalize_output(input: &mut String) {
+    // This warning is emitted by cargo when trying to fetch value; can be either present
+    // or not depending on the user configuration.
+    *input = input.replace("error: config value `http.cainfo` is not set\n", "");
+
+    // `cargo build` output starts with 4 spaces.
+    // The actual output cannot be reliably asserted (e.g. paths can differ, package may compile or not, etc)
+    // so we fully strip any lines that start with 4 spaces.
+    let re = Regex::new(r"^\s{4}.*$").unwrap();
+    *input = input
+        .lines()
+        .filter_map(|line| if re.is_match(line) { None } else { Some(line) })
+        .collect::<Vec<_>>()
+        .join("\n");
 }
